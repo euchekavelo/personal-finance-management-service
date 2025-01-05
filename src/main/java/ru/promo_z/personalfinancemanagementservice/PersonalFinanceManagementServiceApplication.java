f0734fc7ad358bdc097226381531b41703244bdd
Kirill Suslov <euchekavelo@gmail.com>
2025-01-06 02:05:32 +0300

    Added Ð basic project structure.

---
+++ src/main/java/ru/promo_z/personalfinancemanagementservice/PersonalFinanceManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.promo_z.personalfinancemanagementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalFinanceManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalFinanceManagementServiceApplication.class, args);
+	}
+
+}
